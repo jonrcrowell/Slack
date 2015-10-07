943cf00f97ca84d06927a6bd980d70f163590fbc
Steven Murawski <steven.murawski@gmail.com>
2015-10-07 16:57:38 -0500

    initial

---
+++ Slack.psm1
@@ -0,0 +1,57 @@
+$script:SlackApiToken = $env:SlackAPIToken
+
+function Send-SlackApi {
+    param (
+        $Method,
+        $Body = @{},
+        $Token = $script:SlackApiToken
+    )
+    $Body.token = $Token
+    irm "https://slack.com/api/$method" -body $body
+}
+
+function Get-SlackStatus {
+    param ($User)
+    Send-SlackApi -method users.getPresence -body @{user=$User}
+}
+
+function Set-SlackAway {
+    Send-SlackApi -method users.setPresence -body @{presence='away'}
+}
+
+function Set-SlackAuto {
+    Send-SlackApi -method users.setPresence -body @{presence='auto'}
+}
+
+function Send-SlackMessage {
+  param (
+      $text,
+      $channel,
+      $username, 
+      $iconurl, 
+      [switch]$AsUser
+  )
+  end {
+    $body = @{
+      as_user = $AsUser
+      channel = $channel
+    }
+
+    switch ($psboundparameters.keys) {
+      'text'     {$body.text     = $text}
+      'username' {$body.username = $username}
+      'iconurl'  {$body.icon_url = $iconurl}
+    }
+
+    $response = Send-SlackApi -method chat.postMessage -body $body
+    
+    if ($response.ok) {
+      $link = "https://chefio.slack.com/archives/$($response.channel)/" +
+        "p$($response.ts -replace '\.')"
+      $response | 
+        add-member -membertype NoteProperty -Name link -Value $link
+    }
+
+    $response
+  }
+}
